9a314fa10e144bc429076333050c4547f2f14711
Raine-The-Cat <death.donkey@hotmail.com>
2026-01-30 18:01:45 -0800

    added brick breaking sound effects and particles

--- scripts/basic_brick.gd
+++ scripts/basic_brick.gd
@@ -1,14 +1,19 @@
 extends StaticBody2D
 
+var destruction = preload("res://objects/bricks/brick_break.tscn")
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+#	pass
 	
 func _break():
+	var scene = destruction.instantiate()
+	get_parent().add_child(scene)
+	scene.global_position = self.global_position
+	scene.get_node("CPUParticles2D").emitting = true
 	self.queue_free()
