d61c665e74777e0dde28993ebb76f54a2d58e70d
Raine-The-Cat <death.donkey@hotmail.com>
2026-02-04 15:59:58 -0800

    fixed death, added sfx

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -17,19 +17,21 @@ func damage():
 	health -= 1
 	if health == 2:
 		$"Node2D/Heart 1".texture = load("res://sprites/rival heart 2.png")
-
+		_hit()
 	elif health == 1:
 		$"Node2D/Heart 2".texture = load("res://sprites/rival heart 2.png")
-
+		_hit()
 	elif health == 0:
 		$"Node2D/Heart 3".texture = load("res://sprites/rival heart 2.png")
-		$Sprite2D.pause()
+		get_node("Death noise").play()
+		get_node("Sprite2D").stop()
 		defeat.emit()
 
 	elif health <= -1:
 		defeat.emit()
-		
-	get_node("AudioStreamPlayer2D").play()
+
+func _hit():
+	get_node("Hit noise").play()
 	$Sprite2D.pause()
 	$Sprite2D.modulate = Color(1,.5,.5)
 	await get_tree().create_timer(.2).timeout
